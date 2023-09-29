b486bb7b2bb42f19a25a51ccf1e2f76218914257
Pamminger Simon <Simon.PAMMINGER@htlvb.at>
2023-09-29 09:20:55 +0200

    new

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -1,35 +1,30 @@
-﻿Console.WriteLine("== Tiny calculator ==");
+﻿using System.ComponentModel.Design;
 
-int divident;
-while(true)
-{
-    Console.Write("Divident: ");
-    string input = Console.ReadLine();
-    if (int.TryParse(input, out divident))
-    {
-        break;
-       
-    }
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine($"{input} is not a number.");
-    Console.ResetColor();
-}
+Console.WriteLine("== Tiny calculator ==");
 
-int divisor;
-while (true)
-{
-    Console.Write("Divident: ");
-    string input = Console.ReadLine();
-    if (int.TryParse(input, out divisor))
-    {
-        break;
-        
-    }
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine($"{input} is not a number.");
-    Console.ResetColor();
-}
+int dividend = ReadNumber("Dividend");
+int divisor = ReadNumber("Divisor");
+
 
 Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine($"{divident} / {divisor} = {divident / divisor}");
+Console.WriteLine($"{dividend} / {divisor} = {dividend / divisor}");
 Console.ResetColor();
+
+int ReadNumber(string s)
+{
+    int val;
+    while (true)
+    {
+        Console.Write($"{s}: ");
+        string input = Console.ReadLine();
+        if (int.TryParse(input, out val))
+        {
+            return val;
+        }
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{input} is not a number");
+        Console.ResetColor();
+    }
+    
+
+}
