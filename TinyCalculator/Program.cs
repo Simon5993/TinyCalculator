8f63cb046a58d44f7ca3c5f343162586d04777e2
Pamminger Simon <Simon.PAMMINGER@htlvb.at>
2023-09-29 08:44:01 +0200

    Handle division by zero

--- TinyCalculator/Program.cs
+++ TinyCalculator/Program.cs
@@ -1,11 +1,21 @@
 ﻿Console.WriteLine("== Tiny calculator ==");
 
-Console.Write("Divident: ");
-int divident = int.Parse(Console.ReadLine());
+Console.Write("Dividend: ");
+int dividend = int.Parse(Console.ReadLine());
 
 Console.Write("Divisor: ");
 int divisor = int.Parse(Console.ReadLine());
 
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine($"{divident} / {divisor} = {divident / divisor}");
-Console.ResetColor();
+if(divisor == 0)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Divisor must not be zero!");
+    Console.ResetColor();
+}
+
+else
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{dividend} / {divisor} = {dividend / divisor}");
+    Console.ResetColor();
+}
